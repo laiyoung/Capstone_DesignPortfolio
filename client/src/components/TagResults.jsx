f96b37d292fc80acf84e818a8d9c555b56e45fff
laiyoung <l.d.young@outlook.com>
2025-03-17 17:12:09 -0400

    dynamicTagUpdate

--- client/src/components/TagResults.jsx
+++ client/src/components/TagResults.jsx
@@ -7,32 +7,66 @@ export default function TagResults({
   selectedMedium,
   setSelectedMedium,
   fetchPieces,
-  pieces
+  pieces,
 }) {
   const navigate = useNavigate();
+  const imgSmallStyle = {
+    maxWidth: "50%",
+    maxHeight: "50%",
+    borderRadius: "50%",
+    textAlign: "center",
+    margin: "auto",
+    padding: "20px",
+  };
+
   useEffect(() => {
     fetchPieces();
-    setTagResults(pieces.filter((item) => (
-       item.tags.some(tag => tag.medium === selectedMedium))));
+    setTagResults(
+      pieces.filter((item) =>
+        item.tags.some((tag) => tag.medium === selectedMedium)
+      )
+    );
   }, [pieces.length, selectedMedium]);
 
-  // console.log(tagResults)
+  function refreshTagResults(tag) {
+    setSelectedMedium(tag.medium);
+  }
 
   async function handleBack() {
     navigate("/");
   }
 
+ 
   return (
-    <div className="article">
-      <h2> {selectedMedium[0].toUpperCase() + selectedMedium.slice(1)} art pieces: </h2>
-      <button onClick={handleBack}>Back To the Full Gallery</button>
-      {tagResults.map((filteredPiece) => {
-        return (
+    <>
+      <div className="article">
+        <h2>
+          {" "}
+          {selectedMedium[0].toUpperCase() +
+            selectedMedium.slice(1)} pieces:{" "}
+        </h2>
+        <button onClick={handleBack}>Back To the Full Gallery</button>
+
+        {tagResults.map((filteredPiece) => {
+          return (
           <div key={filteredPiece.id} className="single-card-view">
-            <h3>{filteredPiece.title}</h3>
-          </div>
-        );
-      })}
-    </div>
+            <h3>{filteredPiece.title}</h3> 
+            <img
+              style={imgSmallStyle}
+              src={filteredPiece.image_url}
+              alt={filteredPiece.title}
+            />
+            <p>Date: {filteredPiece.date.split("T")[0]} </p>
+            <p>Description: {filteredPiece.description} </p>
+            {filteredPiece.tags &&
+              filteredPiece.tags.map((tag) => (
+                <button key={tag.id} onClick={() => refreshTagResults(tag)}>
+                  {tag.medium}
+                </button>
+              ))}
+          </div>);
+        })}
+      </div>
+    </>
   );
 }
