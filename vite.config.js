1d65388dbe092e5701c72dfbbdd5e0405d77932e
laiyoung <l.d.young@outlook.com>
2025-04-29 15:54:32 -0400

    buildScripts

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ console.log(`api need to be running on port ${serverPort}`);
 
 export default defineConfig({
   plugins: [react()],
-  root: './client',  // Set the root to the client folder
+  root: './client',
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/api': `http://localhost:${serverPort}`,
