a402d9647819c9ac6ffa3f8ccc49ec8931ff29c7
laiyoung <l.d.young@outlook.com>
2025-04-29 19:46:54 -0400

    AddingImportReact

--- client/src/components/TagResults.jsx
+++ client/src/components/TagResults.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React from 'react';
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function TagResults({
--- client/src/components/markerResults.jsx
+++ client/src/components/markerResults.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from 'react';
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../App";
 import { projectsRoutes } from "./Projects/index.js";
