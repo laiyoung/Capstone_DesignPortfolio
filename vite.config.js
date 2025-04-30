25a22640e0af9d21559b2716fc3a9f28f3f7c7c7
laiyoung <l.d.young@outlook.com>
2025-04-29 20:19:54 -0400

    ViteChanges

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 const serverPort = process.env.PORT || 3000;
 console.log(`api need to be running on port ${serverPort}`);
 
 export default defineConfig({
   plugins: [react()],
-  root: './client',
+  root: "./client",
   build: {
-    outDir: 'dist', 
-    emptyOutDir: true
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ["@components"],
+    },
   },
   server: {
     proxy: {
-      '/api': `http://localhost:${serverPort}`,
+      "/api": `http://localhost:${serverPort}`,
     },
   },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "client/src/components"),
+    },
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
 });
-
