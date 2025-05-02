7075295b7a9afc95931505e2ce4a1442d4edd854
laiyoung <l.d.young@outlook.com>
2025-05-01 21:57:14 -0400

    apiURLUpdate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ import {
 } from "./components/Projects/index.js";
 
 /** API Link */
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+export const API_URL = import.meta.env.VITE_API_URL || "/api";
 
 function App() {
   const [error, setError] = useState(null);
